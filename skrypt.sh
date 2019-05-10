3d6b1d46ee3950b4870e3c5c011e0918caa58942
jacekhewko <50452852+jacekhewko@users.noreply.github.com>
2019-05-10 23:27:11 +0200

    Add files via upload

---
+++ skrypt.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo docker-compose up -d && seckey=$(sudo docker-compose run --rm sentry config generate-secret-key) && sudo echo "SENTRY_SECRET_KEY=$seckey" | sudo tee -a .variables > /dev/null && sudo echo "Y jacek.hewko@gmail.com testpass testpass y" | sudo docker-compose run --rm sentry upgrade
